2aebd414b3bf95f2bdb79a5c032b88ec5de31e84
Jake Johnson <johnson.s.jake@gmail.com>
2020-04-10 13:24:39 +0200

    update deploy settings

--- config.rb
+++ config.rb
@@ -46,9 +46,9 @@ configure :build do
 end
 
 activate :deploy do |deploy|
-  deploy.method = :git
+  deploy.deploy_method = :git
   # Optional Settings
-  deploy.remote = 'production' # remote name or git url, default: origin
+  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`

2dee7abd3d45a18871174db8ff5a6854a112aa29
Jake Johnson <johnson.s.jake@gmail.com>
2020-04-10 13:41:37 +0200

    rel links

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+set :relative_links, true
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
@@ -48,7 +50,7 @@ end
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
-  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+  deploy.remote = 'git@github.com:lead-education-group/website-middleman.git' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`

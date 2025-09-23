39a0f75632dadae42d8cb0b96c3b558196406fef
Jake Johnson <johnson.s.jake@gmail.com>
2025-09-24 00:25:10 +0200

    try edge function again 7

--- netlify/edge-functions/force-https.js
+++ netlify/edge-functions/force-https.js
@@ -1,6 +1,8 @@
 export default (request) => {
   const url = new URL(request.url);
 
+  console.log(url);
+  console.log(url.protocol)
   if (url.protocol === "http:") {
     url.protocol = "https:";
     return new Response(null, {
@@ -10,7 +12,7 @@ export default (request) => {
   }
 
   // Let it fall through by returning nothing
-  return new Response("Hello from HTTPS!");
+  // return new Response("Hello from HTTPS!");
 };
 
 export const config = { path: "/*" };

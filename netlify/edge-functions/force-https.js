ec500326770d438365aa7e1d6efca66c7142bfe0
Jake Johnson <johnson.s.jake@gmail.com>
2025-09-24 00:05:39 +0200

    try edge function again 2

--- netlify/edge-functions/force-https.js
+++ netlify/edge-functions/force-https.js
@@ -1,16 +1,15 @@
-import type { Context } from "https://edge.netlify.com";
+export default async function handler(request) {
+  const url = new URL(request.url);
 
-export default async (req: Request, context: Context) => {
-  const url = new URL(req.url);
-
-  // If the request is HTTP, redirect to HTTPS
   if (url.protocol === "http:") {
     url.protocol = "https:";
-    return Response.redirect(null, {
+    return new Response(null, {
       status: 301,
       headers: {
         Location: url.toString(),
       },
     });
   }
-};
+
+  return fetch(request);
+}
